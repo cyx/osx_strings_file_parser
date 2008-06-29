d1965604b38ad6af508ab66d6bdc3fe3d3893bdc
Cyril David <cyx.ucron@gmail.com>
2008-06-30 06:06:10 +0800

    initial commit of osx_strings_file_parser

---
+++ osx_strings_file_parser.rb
@@ -0,0 +1,31 @@
+require 'active_support'
+require 'iconv'
+
+class OsxStringsFileParser
+  def initialize( data )
+    @data = get_utf8_from( data )
+  end
+
+  def to_hash
+    hash = {}
+    @data.chars.split(';').each do |line|
+      next if line.blank?
+      
+      key, value = line.chars.split('=')
+      key = remove_quotes_from(key)
+      value = remove_quotes_from(value)
+
+      hash[key.to_s] = value.to_s
+    end
+    return hash
+  end
+
+  protected
+    def remove_quotes_from( string )
+      string.chars.strip.chomp('"').reverse.chomp('"').reverse                 
+    end
+    
+    def get_utf8_from( data )
+      Iconv.iconv('UTF-8', 'UTF-16', data).to_s
+    end
+end
---
+++ spec/osx_strings_file_parser_spec.rb
@@ -0,0 +1,39 @@
+require 'spec'
+require File.dirname(__FILE__) + '/../osx_strings_file_parser'
+
+describe OsxStringsFileParser do
+  describe "given PropertyName.strings" do
+    before(:each) do
+      @data = File.read( File.dirname(__FILE__) + '/fixtures/PropertyName.strings' )
+    end
+
+    describe "to_hash" do
+      before(:each) do
+        parser = OsxStringsFileParser.new( @data )
+        @hash = parser.to_hash
+      end
+      
+      before(:each) do
+        @test_values = {
+          "Action" => "Action",
+          "Activated Color" => "Activated Color",
+          "Activated State" => "Activated State",
+          "Activated Transp" => "Activated Transp",
+          "Aspect Ratio" => "Aspect Ratio"
+        }
+        
+      end
+      
+      it "should return a 188 sized hash" do
+        @hash.size.should == 188
+      end
+      
+      it "should have the test values present" do
+        @test_values.each do |k, v|
+          @hash[k].should == v
+        end
+      end
+    end
+  end
+end
+
